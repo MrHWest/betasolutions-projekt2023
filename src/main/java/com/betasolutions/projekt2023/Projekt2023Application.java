3beda51d37a320b8157c2a55713efa07fac22ab7
Mikael H <mikael.holsve@gmail.com>
2023-05-11 09:24:11 +0200

    Initial commit

---
+++ src/main/java/com/betasolutions/projekt2023/Projekt2023Application.java
@@ -0,0 +1,13 @@
+package com.betasolutions.projekt2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projekt2023Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Projekt2023Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/betasolutions/projekt2023/Projekt2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betasolutions.projekt2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projekt2023ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
