47dc98b79d1a3de0fcac98ed66a16fa578902ac1
EliasF2 <elia0704@stud.kea.dk>
2023-05-11 13:36:05 +0200

    ConnectioManager oprettet

--- src/main/java/com/betasolutions/projekt2023/utility/ConnectionManager.java
+++ src/main/java/com/betasolutions/projekt2023/utility/ConnectionManager.java
@@ -1,4 +1,23 @@
 package com.betasolutions.projekt2023.utility;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectionManager {
+
+    private static Connection connection = null;
+
+    public static Connection getConnection(String db_url, String uid, String pwd){
+
+        if (connection == null){
+
+            try {
+                connection = DriverManager.getConnection(db_url, uid, pwd);
+            } catch (SQLException e){
+                System.out.println("Could not connect to Database");
+                e.printStackTrace();
+            }
+        }   return connection;
+    }
 }
